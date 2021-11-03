329f8ec3faf45ff6a2810b97a9948fa197f5d59c
mirindas <panagiotis.christodoulou97@gmail.com>
2021-11-03 17:54:21 +0200

    rework on some text

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// const descriptionText = "Entry Level Front-End Developer//";
-
-// let charIndex = 0;
-// let char = "";
-
-// (function typeEffect(){
-//     char = descriptionText.slice(0,++charIndex);
-
-//     document.querySelector(".typeEffect").textContent = char;
-    
-//     setTimeout(typeEffect,400);
-// }());
-
-
-
 const descriptionText = "Entry Level Web Developer//";
 
 let index = 0;
