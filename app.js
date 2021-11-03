178bc778b84a99be7c3e7a75cd87ef252c7fc3ee
mirindas <panagiotis.christodoulou97@gmail.com>
2021-11-03 16:04:48 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// const descriptionText = "Entry Level Front-End Developer//";
+
+// let charIndex = 0;
+// let char = "";
+
+// (function typeEffect(){
+//     char = descriptionText.slice(0,++charIndex);
+
+//     document.querySelector(".typeEffect").textContent = char;
+    
+//     setTimeout(typeEffect,400);
+// }());
+
+
+
+const descriptionText = "Entry Level Front-End Developer//";
+
+let index = 0;
+let letter = "";
+
+(function typeEffect(){
+
+    letter = descriptionText.slice(0,++index);
+    
+    document.querySelector(".typeEffect").textContent = letter;
+ 
+    setTimeout(typeEffect,250);
+}());
