3eec53f71b5070af89e5059315d5e2ec7a3f527e
mirindas <panagiotis.christodoulou97@gmail.com>
2021-11-04 13:22:17 +0200

    added email and created function to copy to clipboard

--- app.js
+++ app.js
@@ -11,3 +11,35 @@ let letter = "";
  
     setTimeout(typeEffect,250);
 }());
+let getMail = document.getElementById("mymail");
+
+getMail.onclick = () =>{
+    copyText();
+    // getMail = popUpStyle(); goes with commented section in popUpStyle (previous implementation)
+    popUpStyle();
+    setTimeout(revertPopUpStyle,1500)   // popUp disappears after x seconds
+}
+
+let message = document.querySelector(".tip-msg")
+
+const popUpStyle = () => {
+
+    message.style.right = "-20px";
+    message.style.opacity = "1";
+    // return `
+    // .tip:after .tip-msg {
+    //     right:-20px;
+    //     opacity:1;
+    // }`
+}
+
+const revertPopUpStyle = () =>{
+    message.style.right = "9999px";
+    message.style.opacity = "0";
+}
+
+const copyText = () => {
+    let mailCopy = document.getElementById("mymail").innerHTML;
+    console.log(mailCopy)
+    navigator.clipboard.writeText(mailCopy)
+}
